87a708f740e17806f7a87db01d6c969277b35282
Shreya K <shreyakdev@gmail.com>
2024-10-05 08:52:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function openNavbar() {
+    document.getElementById("sideNavigationBar")
+        .style.width = "30%";
+}
+function closeNavbar() {
+    document.getElementById("sideNavigationBar")
+        .style.width = "0%";
+}
